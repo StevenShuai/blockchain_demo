f8907502b0ad36a0d7687b9ccf979f20942db48f
StevenShuai <598635259@qq.com>
2025-12-16 09:35:57 +0700

    Update bbqToken.sol

--- contract/bbqToken.sol
+++ contract/bbqToken.sol
@@ -1,1 +1,10 @@
+pragma solidity >=0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BBQToken is ERC20 {
+    
+    constructor() ERC20("barbecue", "BBQ") {
+        _mint(msg.sender, 10000000 * 10 ** decimals());
+    }
+}
