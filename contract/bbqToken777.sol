aabf29aac917d6654b089c4738a760e49d934f7c
StevenShuai <598635259@qq.com>
2025-12-16 23:11:58 +0700

    Update bbqToken777.sol

--- contract/bbqToken777.sol
+++ contract/bbqToken777.sol
@@ -1,1 +1,16 @@
+pragma solidity >=0.8.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.6/contracts/token/ERC777/ERC777.sol";
+
+contract BBQToken is ERC777 {
+
+    constructor()
+        ERC777(
+            "barbecue",
+            "BBQ",
+            new address[](0)
+        )
+    {
+        _mint(msg.sender, 10_000_000 * 10 ** decimals(), "", "");
+    }
+}
